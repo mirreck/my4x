aa2d0db0831ca1aa3503de21a9470299ac86f451
mirreck <noctaudio@gmail.com>
2013-11-22 00:03:41 +0100

    add map

--- my4x-webapp/src/main/java/net/my4x/webapp/controller/MainController.java
+++ my4x-webapp/src/main/java/net/my4x/webapp/controller/MainController.java
@@ -1,6 +1,14 @@
 package net.my4x.webapp.controller;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +27,16 @@ public class MainController {
 	@ResponseBody
 	public String[] data(@PathVariable String id){
 	   return new String[] {"a","b"};
+	}
+	
+	
+	@RequestMapping("rest/maptiles/{x}/{y}/{z}")
+	public ResponseEntity<byte[]> maptiles(@PathVariable String x, @PathVariable String y, @PathVariable String z) throws IOException {
+	    InputStream in = MainController.class.getResourceAsStream("/test.png");
+
+	    final HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.IMAGE_PNG);
+
+	    return new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
 	}
 }
