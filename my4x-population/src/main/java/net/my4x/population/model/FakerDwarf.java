c406c49247f527c96bd1c005e830a3d6a71ee05e
mirreck <noctaudio@gmail.com>
2014-05-02 00:02:30 +0200

    add dwarf names

---
+++ my4x-population/src/main/java/net/my4x/population/model/FakerDwarf.java
@@ -0,0 +1,17 @@
+package net.my4x.population.model;
+
+import java.util.Locale;
+import java.util.Random;
+
+import com.github.javafaker.Faker;
+
+public class FakerDwarf extends Faker {
+	public FakerDwarf() {
+		super(new Locale("dwarf"));
+	}
+
+	public FakerDwarf(Random random) {
+		super(new Locale("dwarf"), random);
+	}
+		   
+}
