bba9c0f86122be625e780a466ea05313746ccb12
Thomas Decoster <thomas.decoster.ext@lafarge.com>
2015-04-17 10:56:20 +0200

    add gradle build

--- my4x-card/src/generated/resources/init/skills.sql
+++ my4x-card/src/generated/resources/init/skills.sql
@@ -1,4 +1,4 @@
 INSERT INTO skills(id,name,level) VALUES ("1","run","0");
 INSERT INTO skills(id,name,level) VALUES ("2","cooking","1");
 INSERT INTO skills(id,name,level) VALUES ("3","punch","1");
-INSERT INTO skills(id,name,level) VALUES ("4","kick","1");
+INSERT INTO skills(id,name,level) VALUES ("4","kick","1")
